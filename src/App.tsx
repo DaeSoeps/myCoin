9a2564bbe5372adc7c8cd6d1762ef636d8e1a3a7
sihwan <karkarson@flexenm.com>
2022-07-12 14:15:51 +0900

    주석 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 const App: React.FC = () => {
-    return (
-        <div>hello world</div>
-    )
+  return (
+    <div>hello world</div>
+    // 스태시 -> 잠시 임시보관소에 저장
+  );
 };
 
 export default App;
