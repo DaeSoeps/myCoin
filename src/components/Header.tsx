4e2c6052b9bcbc7ed518ca93618ddde50e307633
DSS <thdeotjq12@naver.com>
2022-07-27 23:35:22 +0900

    feat: Header 메뉴 css 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../../public/images/coin.png'
+import logo from '../../public/images/coin.png';
+import Image from 'next/image'
 
+console.log("logo", logo)
 const Header = () => {
   return (
     // <Sc.Header>
@@ -19,26 +21,26 @@ const Header = () => {
     // </Sc.Header>
     <HeaderBox>
         <div>
-          <Image width="30px" height="40px" src={logo} />
+          <ImageContainer>
+            <Image width="40px" height="40px" src={logo} />
+            <ImageText>MyCoin</ImageText>
+          </ImageContainer>
         </div>
         <Container1>
           <div>
-            <HeaderSerBtn >
-          
-              <p>검색</p>
-            </HeaderSerBtn>
+            <HeaderMenu >
+              <p>거래소</p>
+            </HeaderMenu>
             </div>
             <div>
-            <HeaderLogBtn >
-            
-              <p>검색</p>
-            </HeaderLogBtn>
+            <HeaderMenu >
+              <p>차트</p>
+            </HeaderMenu>
             </div>
             <div>
-            <HeaderMyBtn >
-          
-              <p>검색</p>
-            </HeaderMyBtn>
+            <HeaderMenu >
+              <p>코인동향</p>
+            </HeaderMenu>
             </div>
 
 
@@ -50,91 +52,31 @@ const Header = () => {
   );
 };
 
-const Sc = {
-  Header: styled.header`
-    position: sticky;
-    height: 65px;
-    width:100%;
-    padding: 1rem;
-    color: white;
-    background: #093687;
-
-    z-index: 100;
-  `,
-  HeaderImg: styled.h3`
-    background-image: url(https://static.upbit.com/logos/BTC.png);
-  `,
-  Title: styled.h2`
-    width: 10%;
-   
-    display: inline-block;
-    float: left
-    margin-top: 21px;
-    color: #fff;
-  `
-}
-
-const Wrap = {
-  ContentWrap: styled.div`
-    background-color: #e9ecf1;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `,
-  Section: styled.section`
-  width: 10%;
-   
-  display: inline-block;
-  float: left
-  margin-top: 21px;
-  color: #fff;
-  `,
-  Nav: styled.nav`
-  margin-left: 20px;
-  `,
-  Ul: styled.ul`
-
-  `,
-  Li: styled.li`
-    float: left;
-    height: 79px;
-    line-height: 79px;
-  `,
-  A: styled.a`
-    position: relative;
-    font-size: 15px;  
-    font-family: Roboto,Noto Sans KR,sans-serif,Arial,Helvetica;
-    text-decoration: none;
-  `,
-  Title: styled.h2`
-  width: 10%;
- 
-  float: left
-  margin-top: 21px;
-  color: #fff;
-`
-
-
-}
 
 const HeaderBox = styled.div`
-  height: 10.55vh;
-  width: 100%;
+  height: 7.55vh;
+  width: 95%;
   top: 0;
-  padding: 0 16.67vw;
+  padding: 0 5.67vw;
   position: fixed;
   z-index: 10;
   display: flex;
-  justify-content: space-between;
+
   align-items: center;
   background: #093687;
   border-bottom: 2px solid #eeeeee;
 
 `;
 
-const HeaderSerBtn = styled.button`
-  width: 28px;
+const HeaderMenu = styled.button`
+  width: 100%;
   height: 37px;
+  border: 0;
+  outline: 0;
+  background-color: transparent;
+  color: white;
+  cursor:pointer;
+  font-weight : bold ;
   margin-right: 10px;
   display: flex;
   flex-direction: column;
@@ -144,58 +86,23 @@ const HeaderSerBtn = styled.button`
     font-size: 14px;
   }
 `;
-const HeaderLogBtn = styled.button`
-  width: 56px;
-  height: 37px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  & > p {
-    margin-top: 8px;
-    font-size: 14px;
-  }
-`;
-const HeaderMyBtn = styled.button`
-  width: 70px;
-  height: 37px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  & > p {
-    margin-top: 8px;
-    font-size: 14px;
-  }
-`;
+
 const Container1 = styled.div`
   width: 350px;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
 `;
+const ImageContainer = styled.div`
+  flex-direction: column;
+  display: flex;
+`
+const ImageText = styled.a`
+  color : white;
+  flex-direction: column;
+  display: flex;
+`
 
-const Image = styled.img`
-  width: ${(props) => (props.width ? props.width : "17.8em")};
-  height: ${(props) => (props.height ? props.height : "14.81vh")};
-  src:  ${(props) => (props.src ? props.src : "")};
 
-`;
-
-Image.defaultProps = {
-  width: "",
-  height: "",
-  borderRadius: "",
-  border: "",
-  margin: "",
-  marbleBorder: "",
-  maxHeight: "",
-  mobileMaxHeight: "",
-  chatClose: "",
-  post: "",
-  mypage: "",
-  no_cursor: "",
-  onClick: () => {},
-  alt: "",
-  src: "",
-};
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,17 @@ module.exports = (env, argv) => {
         test: /\.tsx?$/,
         use: ["babel-loader", "ts-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options:{
+              esModule: false,
+            },
+          },
+        ],
+      }
     ],
   },
 
