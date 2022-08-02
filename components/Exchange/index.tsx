c20c5c1998c3fc905f9abf06cbcadbdeade1a4e7
DSS <thdeotjq12@naver.com>
2022-08-02 23:30:47 +0900

    feat: SongPod - 거래소 부분 구현 진행중

--- components/Exchange/index.tsx
+++ components/Exchange/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import * as ST from './style'
 const Exchange = () => {
-  return <div>거래소컴포넌트거래소컴포넌트거래소컴포넌트거래소컴포넌트</div>
+  return (
+    <>
+      <ST.MainDiv>
+        <ST.SectionLeft>
+          <ST.ArtA.Art>
+            <ST.ArtA.Title>비트코인</ST.ArtA.Title>
+            <ST.ArtA.Body>
+              <ST.ArtA.BodyA></ST.ArtA.BodyA>
+              <ST.ArtA.BodyB></ST.ArtA.BodyB>
+              <ST.ArtA.BodyC></ST.ArtA.BodyC>
+            </ST.ArtA.Body>
+
+          </ST.ArtA.Art>
+          <ST.ArtB></ST.ArtB>
+        </ST.SectionLeft>
+        <ST.SectionRight>
+
+        </ST.SectionRight>
+      </ST.MainDiv>
+    </>
+
+  )
 }
 
 export default Exchange
---
+++ components/Exchange/style.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const MainDiv = styled.div`
+  padding-top: 72px;
+  margin: 0 auto;
+`
+export const SectionLeft = styled.section`
+
+`
+
+export const SectionRight = styled.section`
+
+`
+
+export const ArtA = {
+  Art: styled.article`
+  float: left;
+  width: 100%;
+  margin-bottom: 10px;
+  background-color: #fff;
+  `,
+  Title: styled.span`
+  position: relative;
+  display: block;
+  height: 44px;
+  padding: 0 14px;
+  border-bottom: 1px solid #ddd;
+  `,
+  Body: styled.div`
+  display: block
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  `,
+  BodyA: styled.span`
+  position: relative;
+  display: block;
+  padding: 18px 20px 14px;
+  overflow: hidden;
+  `,
+  BodyB: styled.span`
+  display: block;
+  height: 18px;
+  padding: 7px 20px;
+  background-color: #f9fafc;
+  border-top: 1px solid #f4f5f8;
+  overflow: hidden;
+  `,
+  BodyC: styled.span`
+  height: 451px;
+  display: block;
+  width: 100%;
+  border-top: 1px solid #ddd;
+  overflow: hidden;
+  `,
+}
+
+export const ArtB = styled.article`
+
+`
